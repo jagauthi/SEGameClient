c70b6a398b100cff658a61ff12df86a5c8ac11a6
Joseph <joseph.a.gauthier@gmail.com>
2016-02-17 16:52:04 -0600

    Update Main.java

--- src/client_Controller/Main.java
+++ src/client_Controller/Main.java
@@ -1,34 +1,10 @@
 package client_Controller;
 
-import client_Model.*;
 import client_View.Launcher;
 
-import java.awt.Color;
-
-import javax.swing.JFrame;
-
-public class Main extends JFrame{
-	
-	private static final long serialVersionUID = 1L;
-  
-	final int WIDTH = 1280;
-	final int HEIGHT = 720;
-	
-	Player me;
-
-	public Main(){
-		me = new Player(this);
-		addKeyListener(new KeyListener(this, me));
-		setTitle("SE Game");
-		setSize(WIDTH, HEIGHT);
-		setResizable(false);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBackground(Color.GREEN);
-	} 
+public class Main{
 	
  	public static void main(String[] args) throws Exception {
  		new Launcher();
     }
- 	
 }
