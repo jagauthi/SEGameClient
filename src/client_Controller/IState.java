1dd61189544172e787afa5c29c600b1db3c59677
Joseph <joseph.a.gauthier@gmail.com>
2016-02-17 16:51:46 -0600

    Update IState.java

--- src/client_Controller/IState.java
+++ src/client_Controller/IState.java
@@ -1,8 +1,44 @@
 package client_Controller;
-public interface IState
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+
+import client_Model.Player;
+
+public class IState
 {
-    public void update();
-    public void render();
-    public void onEnter();
-    public void onExit();
+	Player player; 
+	Image image; 
+	Graphics graphics; 
+	JFrame frame;
+	
+	public IState(Player p, Image i, Graphics g, JFrame f)
+	{
+		player = p;
+		image = i;
+		graphics = g;
+		frame = f;
+	}
+	
+    public void update()
+    {
+    	
+    }
+    
+    public void render()
+    {
+    	
+    }
+    
+    public void onEnter()
+    {
+    	
+    }
+    
+    public void onExit()
+    {
+    	
+    }
 }
