6d56eb04cb9961b7e8f44f316ab49edaa556e676
jhunt10 <jxh074@uark.edu>
2016-02-17 18:58:00 -0600

    Update KeyListener.java

--- src/client_Controller/KeyListener.java
+++ src/client_Controller/KeyListener.java
@@ -8,56 +8,21 @@ import client_Model.Player;
 public class KeyListener extends KeyAdapter{
 	
 	Player player;
-	Main game;
+	StateMachine sm;
 
-	public KeyListener(Main g, Player me) {
-		game = g;
-		player = me;
+	public KeyListener(StateMachine s) {
+		sm = s;
 	}
  
 	public void keyPressed(KeyEvent e)
 	{
-		int keyCode = e.getKeyCode();
-			
-		if(keyCode == KeyEvent.VK_W){
-			player.setYDirection(-6);
-		}
-			
-		if(keyCode == KeyEvent.VK_A){
-			player.setXDirection(-6);
-		}
-			
-		if(keyCode == KeyEvent.VK_S){
-			player.setYDirection(6);
-		}
-			
-		if(keyCode == KeyEvent.VK_D){
-			player.setXDirection(6);
-		}
-		
-		if(keyCode == KeyEvent.VK_SPACE){
-
-		}
+		int keyCode = e.getKeyCode();		
+		sm.mCurrentState.keyPressed(keyCode);
 	}
 	
 	public void keyReleased(KeyEvent e)
 	{
 		int keyCode = e.getKeyCode();
-	
-		if(keyCode == KeyEvent.VK_W){
-			player.setYDirection(0);
-		}
-		
-		if(keyCode == KeyEvent.VK_A){
-			player.setXDirection(0);
-		}
-		
-		if(keyCode == KeyEvent.VK_S){
-			player.setYDirection(0);
-		}
-		
-		if(keyCode == KeyEvent.VK_D){
-			player.setXDirection(0);
-		}
+		sm.mCurrentState.keyReleased(keyCode);
 	}
 }
