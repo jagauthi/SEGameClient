351de26eeb70ee13e97f93a1d512668bb843aad3
jhunt10 <jxh074@uark.edu>
2016-02-17 18:55:51 -0600

    Update Main.java

--- src/client_Controller/Main.java
+++ src/client_Controller/Main.java
@@ -5,6 +5,7 @@ import client_View.Launcher;
 public class Main{
 	
  	public static void main(String[] args) throws Exception {
- 		new Launcher();
+ 		//new Launcher();
+ 		new StateMachine("Dummby Info");
     }
 }
