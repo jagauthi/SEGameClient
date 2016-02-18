6fd371e4dfdad88690ba1964e74d44daddad9cbf
jhunt10 <jxh074@uark.edu>
2016-02-17 18:57:14 -0600

    Update GameFrame.java

--- src/client_View/GameFrame.java
+++ src/client_View/GameFrame.java
@@ -1,1 +1,31 @@
+package client_View;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	
+	private Image dbImage;
+	private Graphics dbGraphics;
+	
+	public GameFrame(){
+		
+	}
+	
+	public void paint(Graphics g){
+		dbImage = createImage(getWidth(), getHeight());
+		dbGraphics = dbImage.getGraphics();
+		paintComponent(dbGraphics);
+		g.drawImage(dbImage, 0, 0, this);
+	}
+	
+	public void paintComponent(Graphics g){
+		g.setColor(Color.black);
+		//g.fillRect(0, 0, 300, 300);
+	}
+}
 
