7dbd9bd1c2b182fcf911bbce84823d84b352c3a7
jhunt10 <jxh074@uark.edu>
2016-02-18 13:19:59 -0600

    Create IState_new.java

---
+++ src/client_Controller/IState_new.java
@@ -0,0 +1,42 @@
+package client_Controller;
+
+import java.awt.Graphics;
+import client_Model.Player;
+
+public class IState
+{
+	Player player;
+	
+	public IState(Player p)
+	{
+		player = p;
+	}
+	
+    public void update()
+    {
+    	
+    }
+    
+    public void render(Graphics g)
+    {
+    	
+    }
+    
+    public void onEnter()
+    {
+    	
+    }
+    
+    public void onExit()
+    {
+    	
+    }
+    
+    public void keyPressed(int keyCode){
+
+    }
+    
+    public void keyReleased(int keyCode){
+    	
+    }
+}
