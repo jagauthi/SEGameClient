58d9ed5d9490785fc611eac1da4a68faa0481f94
jhunt10 <jxh074@uark.edu>
2016-02-17 18:56:40 -0600

    Update CountryViewState.java

--- src/client_Controller/CountryViewState.java
+++ src/client_Controller/CountryViewState.java
@@ -1,17 +1,32 @@
 package client_Controller;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+
 
 import client_Model.Player;
 
 public class CountryViewState extends IState
 {
-	public CountryViewState(Player p, Image i, Graphics g, JFrame f)
+	private BufferedImage backGround;
+	public CountryViewState(Player p, JFrame f)
 	{
-		super(p, i, g, f);
+		super(p, f);
+		try {
+			backGround = ImageIO.read(new File("resources/world.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 	}
 	
     public void update()
@@ -19,9 +34,12 @@ public class CountryViewState extends IState
         // Nothing to update in the empty state.
     }
   
-    public void render()
+    public void render(Graphics g)
     {
-        // Nothing to render in the empty state
+    	Color colorBrown = new Color(0.7f, 0.45f, 0.1f);
+		g.setColor(colorBrown);
+		g.fillRect(0, 0, 100, 100);
+//    	g.drawImage(backGround, 100, 100, null);
     }
   
     public void onEnter()
