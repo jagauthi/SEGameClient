4fb9c67bc3c6a83b4ae9fb60a3809cdf198a6f83
Joseph <joseph.a.gauthier@gmail.com>
2016-02-18 18:20:38 -0600

    Update Main.java

--- src/client_Controller/Main.java
+++ src/client_Controller/Main.java
@@ -1,11 +1,19 @@
 package client_Controller;
 
+import javax.swing.JFrame;
+
+import client_View.GamePanel;
 import client_View.Launcher;
 
 public class Main{
 	
  	public static void main(String[] args) throws Exception {
- 		//new Launcher();
- 		new StateMachine("Dummby Info");
+ 		new Launcher();
+// 		JFrame window = new JFrame("SWE Game");
+// 		window.setContentPane(new GamePanel());
+// 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// 		window.setResizable(false);
+// 		window.pack();
+// 		window.setVisible(true);
     }
 }
