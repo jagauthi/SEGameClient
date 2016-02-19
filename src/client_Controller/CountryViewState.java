63fd876c02f73296d23e7b4c2bac19ec856e52a0
Joseph <joseph.a.gauthier@gmail.com>
2016-02-18 18:21:07 -0600

    Update CountryViewState.java

--- src/client_Controller/CountryViewState.java
+++ src/client_Controller/CountryViewState.java
@@ -2,58 +2,74 @@ package client_Controller;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-
+import java.awt.event.KeyEvent;
 import client_Model.Player;
-import client_View.GameFrame;
+import client_View.GamePanel;
 
 public class CountryViewState extends IState
 {
-	int x = 1;
-	private BufferedImage backGround;
-	public CountryViewState(Player p, GameFrame f)
+	public CountryViewState(Player p)
 	{
-		super(p, f);
-		try {
-			backGround = ImageIO.read(new File("resources/map.jpg"));
-		} 
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		
+		super(p);
 	}
 	
     public void update()
     {
-    	
+        player.update();
     }
   
     public void render(Graphics g)
     {
-    	Color colorBrown = new Color(0.7f, 0.45f, 0.1f);
-		g.setColor(colorBrown);
-		g.fillRect(0, 0, 300, 100);
-		
-    	g.drawImage(backGround, -x, -x, null);
-    	x++;
-		//screen.paintComponent(g);
+    	g.setColor(Color.green);
+    	g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+    	player.draw(g);
+    	g.setColor(Color.black);
+    	g.drawString("Name: " + player.name, 10, 10);
+    	g.drawString("Class: " + player.playerClass, 60, 60);
     }
   
     public void onEnter()
     {
-    	
+        // No action to take when the state is entered
     }
   
     public void onExit()
     {
+        // No action to take when the state is exited
+    }
+    
+    public void keyPressed(int keyCode){
+    	if(keyCode == KeyEvent.VK_W){
+			player.moveUp();
+			player.stopDown();
+		}
+    	if(keyCode == KeyEvent.VK_A){
+    		player.moveLeft();
+    		player.stopRight();
+		}
+    	if(keyCode == KeyEvent.VK_S){
+    		player.moveDown();
+    		player.stopUp();
+		}
+    	if(keyCode == KeyEvent.VK_D){
+    		player.moveRight();
+    		player.stopLeft();
+		}
     	
+    }
+    
+    public void keyReleased(int keyCode){
+    	if(keyCode == KeyEvent.VK_W){
+			player.stopUp();
+		}
+    	if(keyCode == KeyEvent.VK_A){
+    		player.stopLeft();
+		}
+    	if(keyCode == KeyEvent.VK_S){
+    		player.stopDown();
+		}
+    	if(keyCode == KeyEvent.VK_D){
+    		player.stopRight();
+		}
     }
 }
