51909c7ac694f36d50cdbbb7dffea9c7cf5934d1
Joseph <joseph.a.gauthier@gmail.com>
2016-02-18 19:58:31 -0600

    Update EmptyState.java

--- src/client_Controller/EmptyState.java
+++ src/client_Controller/EmptyState.java
@@ -1,164 +1,31 @@
 package client_Controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import client_Model.Player;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
-import client_View.Launcher;
-
-public class ChatClient extends Thread{
- 
-    BufferedReader in;
-    PrintWriter out;
-    public JFrame frame = new JFrame("Chatter");
-    JTextField textField = new JTextField(40);
-    JTextArea messageArea = new JTextArea(8, 40);
-    Launcher launcher;
-    StateMachine sm;
-    
-    public ChatClient(Launcher launch) {
-    	launcher = launch;
-        // Layout GUI
-        textField.setEditable(false);
-        messageArea.setEditable(false);
-        frame.getContentPane().add(textField, "North");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setVisible(true);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-
-        // Add Listeners
-        textField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                out.println(textField.getText());
-                textField.setText("");
-            }
-        });
-    }
-    
-    public void setStateMachine(StateMachine sm)
+public class EmptyState extends IState
+{
+	
+	public EmptyState(Player p)
+	{
+		super(p);
+	}
+    public void update()
     {
-    	this.sm = sm;
+        // Nothing to update in the empty state.
     }
-
-    private String getServerAddress() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
-    }
-
-    public String getScreenName() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Choose a screen name:",
-            "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
-    }
-
-    public void run() {
-        // Make connection and initialize streams
-    	try{
-	        String serverAddress = getServerAddress();
-	        Socket socket = new Socket(serverAddress, 9001);
-	        in = new BufferedReader(new InputStreamReader(
-	            socket.getInputStream()));
-	        out = new PrintWriter(socket.getOutputStream(), true);
-
-	        // Process all messages from server, according to the protocol.
-	        while (true) {
-	            String line = in.readLine();
-	            String[] message = line.split(":");
-	            if (message[0].equals("SUBMITNAME")) {
-	                out.println(getScreenName());
-	            } 
-	            else if (message[0].equals("NAMEACCEPTED")) {
-	                textField.setEditable(true);
-	            } 
-	            else if (message[0].equals("MESSAGE")) {
-	                messageArea.append(line.substring(8) + "\n");
-	            }
-	            else if (message[0].equals("loginSuccess")) { 
-	                //The server should send each of the characters that belong to 
-	                //our account. All the information in each character will be separated
-	                //with spaces (or some other delimiter) and each of the individual
-	                //characters will be separated by a colon (or some other delimiter)
-	            	launcher.loadCharacterInfo(message);
-	                launcher.initCharSelectPanel();
-	                launcher.switchCards("Char Select Panel");
-	            }
-	            else if (message[0].equals("usernameNotFound")) { 
-	                launcher.usernameNotFound();
-	            }
-	            else if (message[0].equals("accountLocked")) { 
-	                launcher.accountLocked();
-	            }
-	            else if (message[0].equals("accountBanned")) { 
-	                launcher.accountBanned();
-	            }
-	            else if (message[0].equals("incorrectPassword")) { 
-	                launcher.incorrectPassword();
-	            }
-	            else if (message[0].equals("moreThanOneAccountFound")) { 
-	                launcher.moreThanOneAccountFound();
-	            }
-	            else if (message[0].equals("accountAlreadyExists")) { 
-	                launcher.accountAlreadyExists();
-	            }
-	            else if (message[0].equals("accountCreated")) { 
-	                launcher.accountCreated();
-	            }
-	            else if (message[0].equals("charCreatedInventorySuccess")) { 
-	            	launcher.loadCharacterInfo(message);
-	                launcher.initCharSelectPanel();
-	                launcher.switchCards("Char Select Panel");
-	            }
-	            else if (message[0].equals("securityInfo")) { 
-	            	launcher.manageAccount(message);
-	            }
-	            else if (message[0].equals("charInfoDeleted")) { 
-	            	launcher.loadCharacterInfo(message);
-	                launcher.initCharSelectPanel();
-	                launcher.switchCards("Login Panel");
-	                launcher.switchCards("Char Select Panel");
-	            }
-	            else if (message[0].equals("accountUpdated")) { 
-	            	launcher.backToCharSelect();
-	            }
-	            else if (message[0].equals("characterInfo")) { 
-	            	//name, class, level, gender, health, mana, experience, xCoord, yCoord, gold, strength, dexterituy, constitution, intelgence, willpower, luck, abilities, cooldown
-	            	launcher.intoGame(message);
-	            }
-	            else if (message[0].equals("charUpdated")) { 
-	            	//charUpdated:char1Name char1x char1y:char2Name char2x char2y:...
-	            	sm.updateCharsAroundMe(message);
-	            }
-	            else
-	            {
-	            	System.out.println("Not sure how to handle this error... the error is: ");
-	            	System.out.println(message[0]);
-	            }
-	        }
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
-    }
-    
-    public void sendMessage(String message)
+  
+    public void render()
     {
-    	out.println(message);
+        // Nothing to render in the empty state
+    }
+  
+    public void onEnter()
+    {
+        // No action to take when the state is entered
+    }
+  
+    public void onExit()
+    {
+        // No action to take when the state is exited
     }
 }
