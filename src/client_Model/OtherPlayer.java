fa56d2a72c6c79e4257f4e7aaee583b90f3518b2
Joseph <joseph.a.gauthier@gmail.com>
2016-02-19 11:11:27 -0600

    Update OtherPlayer.java

--- src/client_Model/OtherPlayer.java
+++ src/client_Model/OtherPlayer.java
@@ -5,14 +5,15 @@ import java.awt.Rectangle;
 public class OtherPlayer {
 	
 	Rectangle playerRect;
-	String name;
+	String name, playerClass;
 	int x, y;
 	final int WIDTH = 40;
 	final int HEIGHT = 60;
 	
-	public OtherPlayer(String n, int x, int y)
+	public OtherPlayer(String n, String pc, int x, int y)
 	{
 		name = n;
+		playerClass = pc;
 		this.x = x;
 		this.y = y;
 		playerRect = new Rectangle(x, y, WIDTH, HEIGHT);
@@ -21,6 +22,16 @@ public class OtherPlayer {
 	public Rectangle getPlayerRect()
 	{
 		return playerRect;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getPlayerClass()
+	{
+		return playerClass;
 	}
 	
 	public int getX()
