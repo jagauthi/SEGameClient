1e362f20e0a2c85839d9e2adf6d87e82e24980fb
Joseph <joseph.a.gauthier@gmail.com>
2016-02-20 10:28:42 -0600

    Update Location.java

--- src/client_Model/Location.java
+++ src/client_Model/Location.java
@@ -1,18 +1,40 @@
 package client_Model;
 
+import java.awt.Rectangle;
+
 public class Location {
 	
 	String name;
-	int x, y, width, height;
+	int x, y;
+	public final static int WIDTH = 80;
+	public final static int HEIGHT = 80;
+	Rectangle locRect;
 	
-	public Location(String n, int x, int y, int w, int h)
+	public Location(String n, int x, int y)
 	{
 		name = n;
 		this.x = x;
 		this.y = y;
-		width = w;
-		height = h;
+		locRect = new Rectangle(x, y, WIDTH, HEIGHT);
+	}
+	
+	public String getName()
+	{
+		return name;
 	}
 
+	public int getX()
+	{
+		return x;
+	}
 	
+	public int getY()
+	{
+		return y;
+	}
+	
+	public Rectangle getLocRect()
+	{
+		return locRect;
+	}
 }
