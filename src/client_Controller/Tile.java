5afb4873631964d3eaa8136d969ac019b662c9ef
Joseph <joseph.a.gauthier@gmail.com>
2016-02-25 19:42:52 -0600

    Update Tile.java

--- src/client_Controller/Tile.java
+++ src/client_Controller/Tile.java
@@ -5,6 +5,8 @@ import java.awt.Image;
 public class Tile {
 	
 	int x, y;
+	public static final int WIDTH = 40;
+	public static final int HEIGHT = 40;
 	Image tileImage;
 	
 	public Tile(int newX, int newY)
