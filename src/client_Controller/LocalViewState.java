f2d1347515b17435fe19258a6cf6205940a9ba76
Joseph <joseph.a.gauthier@gmail.com>
2016-02-26 13:27:48 -0600

    Update LocalViewState.java

--- src/client_Controller/LocalViewState.java
+++ src/client_Controller/LocalViewState.java
@@ -2,13 +2,18 @@ package client_Controller;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+
+import client_Model.OtherPlayer;
 import client_Model.Player;
 import client_View.GamePanel;
 
 public class LocalViewState extends IState{
 	
 	int xOffset, yOffset = 0;
+	int playerEnterX, playerEnterY, playerExitX, playerExitY = 0;
+	Rectangle exitBox;
 	
 	public LocalViewState(Player p, StateMachine s)
 	{
@@ -17,7 +22,15 @@ public class LocalViewState extends IState{
 	
     public void update()
     {
-       // player.update();
+    	if(exitBox != null)
+    	{
+	    	if(player.getPlayerRect().intersects(exitBox))
+	    	{
+	    		String[] args = new String[1];
+	    		args[0] = "CountryViewState";
+	    		sm.changeState(args);
+	    	}
+    	}
     }
     
     public void oncePerSecondUpdate()
@@ -35,10 +48,15 @@ public class LocalViewState extends IState{
     	player.draw(g);
     	g.setColor(Color.lightGray);
     	g.fillRect(0, 600, 300, 120);
+    	g.setColor(Color.black);
     	g.drawString("Name: " + player.getName(), 10, 610);
     	g.drawString("HP: " + player.getHealth(), 10, 630);
     	g.drawString("Mana: " + player.getMana(), 10, 650);
+    	g.drawString("Coords: " + player.getX() + ", " + player.getY(), 10, 670);
     	
+    	g.setColor(Color.BLACK);
+    	if(exitBox != null)
+    		g.fillRect(exitBox.x*40 - xOffset, exitBox.y*40 - yOffset, exitBox.width*40, exitBox.height*40);
     	drawOtherPlayers(g);
     }
     
@@ -46,24 +64,26 @@ public class LocalViewState extends IState{
     {
     	if(StateMachine.otherPlayers.size() > 0)
     	{
-    		g.setColor(Color.red);
     		for(int i = 0; i < StateMachine.otherPlayers.size(); i++)
     		{
-    			if(StateMachine.otherPlayers.get(i).getPlayerClass().equals("Rogue"))
+    			OtherPlayer other = StateMachine.otherPlayers.get(i);
+    			if(other.getPlayerClass().equals("Rogue"))
     			{
     				g.setColor(Color.green);
     			}
-    			else if(StateMachine.otherPlayers.get(i).getPlayerClass().equals("Mage"))
+    			else if(other.getPlayerClass().equals("Mage"))
     				g.setColor(Color.blue);
     			else
     				g.setColor(Color.red);
     			
-    			if(!StateMachine.otherPlayers.get(i).getName().equals(player.getName()))
+    			if(!other.getName().equals(player.getName()))
     			{
 	    			g.fillRect(StateMachine.otherPlayers.get(i).getX()*40 - xOffset, 
 	    					StateMachine.otherPlayers.get(i).getY()*40- yOffset, 
 	    					StateMachine.otherPlayers.get(i).getWidth(), 
 	    					StateMachine.otherPlayers.get(i).getHeight());
+	    			g.setColor(Color.WHITE);
+	    			g.drawString(other.getName(), other.getX()*40-xOffset, other.getY()*40-yOffset+10);
     			}
     		}
     	}
@@ -71,28 +91,35 @@ public class LocalViewState extends IState{
   
     public void onEnter()
     {
-        /*
-         * We should make it so that the player enters the location at the same
-         * spot every time, like maybe it always loads the player into
-         * the bottom middle side of the town or something.
-         */
+        player.setX(playerEnterX);
+        player.setY(playerEnterY);
     }
   
     public void onExit()
     {
         // No action to take when the state is exited
+    	player.setX(playerExitX);
+    	player.setY(playerExitY);
     }
     
     public void loadInfo(String[] info)
     {
-    	/*
-    	 * Call the database and send it info[1], that is the
-    	 * name of this location. Then it will send back info
-    	 * like all the npcs and stuff. Maybe this needs
-    	 * to go into another method, so that when the chat client
-    	 * sends us all that info, we can load it in this method...
-    	 * Not sure yet.
-    	 */
+    	//This is where we get all the info about the location we have just entered.
+    	for(int i = 0; i < info.length; i++)
+    	{
+    		System.out.println(i + " " + info[i]);
+    	}
+    	exitBox = new Rectangle(Integer.parseInt(info[10]), Integer.parseInt(info[11]), 2, 2);
+    	playerEnterX = Integer.parseInt(info[14]);
+    	playerEnterY = Integer.parseInt(info[15]);
+    	playerExitX = Integer.parseInt(info[12]);
+    	playerExitY = Integer.parseInt(info[13]);
+    	//info[10] holds the exit box X
+    	//info[11] holds the exit box Y
+    	//info[12] holds where the player will be when he goes back to the country view X
+    	//info[13] holds where the player will be when he goes back to the country view Y
+    	//info[14] holds where the player will enter X
+    	//info[15] holds where the player will enter Y
     }
     
     public void keyPressed(int keyCode){
