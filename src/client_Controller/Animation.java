9f5eff68bec4abb7d895bc410eca0cbb40d1af88
jhunt10 <jxh074@uark.edu>
2016-02-26 18:50:41 -0600

    Update Animation.java

--- src/client_Controller/Animation.java
+++ src/client_Controller/Animation.java
@@ -7,8 +7,10 @@ public class Animation {
 	private int currentFrame;
 	private long startTime;
 	private long delay;
+	int[] offsetX, offsetY;
 	
 	private Boolean playedOnce;
+	private Boolean repeat;
 	
 	public Animation(){
 		playedOnce = false;
@@ -17,26 +19,40 @@ public class Animation {
 	public void setFrames(BufferedImage[] frameIn){
 		this.frames = frameIn;
 		startTime = System.nanoTime();
-		playedOnce = false;
+		
+		repeat = false;
+		currentFrame = 0;
 	}
 	
 	public void setDelay(long delay){this.delay = delay;}
 	public void setCurrentFrame(int n){this.currentFrame = n;}
+	public void setOffsetX(int[] n){offsetX = n;}
+	public void setOffsetY(int[] n){offsetY = n;}
+	public void setRepeat(boolean b){repeat = b;}
+	
+	public void resetHasPlayedOnce(){playedOnce = false;}
+	
 	public int getFrame(){return currentFrame;}
 	public BufferedImage getImage(){return frames[currentFrame];}
 	public boolean getHasPlayedOnce(){return playedOnce;}
+	public int getOffsetX(){return offsetX[currentFrame];}
+	public int getOffsetY(){return offsetY[currentFrame];}
 	
 	public void update(){
 		if(delay == -1) return;
-		
 		long elipsed = (System.nanoTime() - startTime)/1000000;
 		if(elipsed > delay){
 			currentFrame++;
 			startTime = System.nanoTime();
 		}
 		if(currentFrame == frames.length){
-			currentFrame = 0;
 			playedOnce = true;
+			if(repeat){
+				currentFrame = 0;
+			} else {
+				currentFrame = frames.length - 1;
+			}
+			
 		}
 	}
 }
