066f526be14b85d533d2cf6b9ad12e964689fb5b
Joseph <joseph.a.gauthier@gmail.com>
2016-03-03 19:23:02 -0600

    Update Tile.java

--- src/client_Controller/Tile.java
+++ src/client_Controller/Tile.java
@@ -4,15 +4,37 @@ import java.awt.Image;
 
 public class Tile {
 	
-	int x, y;
+	int x, y, type;
+	double randomEncounterChance = 0.0;
 	public static final int WIDTH = 40;
 	public static final int HEIGHT = 40;
 	Image tileImage;
 	
-	public Tile(int newX, int newY)
+	public Tile(int newX, int newY, int newType)
 	{
 		x = newX;
 		y = newY;
+		type = newType;
+		
+		if(type == 2)
+			randomEncounterChance = 1.0;
+		else if(type == 3)
+			randomEncounterChance = 2.0;
+		else if(type == 4)
+			randomEncounterChance = 3.0;
+		else if(type == 5)
+			randomEncounterChance = 4.0;
+		else
+			randomEncounterChance = 0;
 	}
 
+	public int getType()
+	{
+		return type;
+	}
+	
+	public double getRandomEncounterChance()
+	{
+		return randomEncounterChance;
+	}
 }
