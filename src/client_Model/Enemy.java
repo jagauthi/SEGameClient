bb373fb1f873183dd384159fc85ef60a043a1f2a
Joseph <joseph.a.gauthier@gmail.com>
2016-03-15 13:33:11 -0500

    Update Enemy.java

--- src/client_Model/Enemy.java
+++ src/client_Model/Enemy.java
@@ -1,14 +1,30 @@
 package client_Model;
 
+import java.awt.Rectangle;
+
 public class Enemy {
 	
 	String name;
 	int health, damage;
+	Rectangle enemyRect;
+	public static int WIDTH = 30;
+	public static int HEIGHT = 60;
 	
 	public Enemy(String newName, int newHealth, int newDamage)
 	{
 		name = newName;
 		health = newHealth;
 		damage = newDamage;
+		enemyRect = new Rectangle();
+	}
+	
+	public Rectangle getRect()
+	{
+		return enemyRect;
+	}
+	
+	public void setRect(int x, int y, int width, int height)
+	{
+		enemyRect.setBounds(x, y, width, height);
 	}
 }
