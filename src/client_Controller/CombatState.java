7bebdec4ee4b00440248353bc63aaf2d2d236a8b
Joseph <joseph.a.gauthier@gmail.com>
2016-03-15 13:30:22 -0500

    Update CombatState.java

--- src/client_Controller/CombatState.java
+++ src/client_Controller/CombatState.java
@@ -2,6 +2,7 @@ package client_Controller;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -36,70 +37,9 @@ public class CombatState extends IState
 		catch (IOException ioe) {
             System.out.println("Unable to load image file.");
         }
-	}
-	
-	public void attackMenu()
-	{
 		
-	}
-	
-	public void magicMenu()
-	{
-		
-	}
-	
-	public void inventoryMenu()
-	{
-		
-	}
-	
-	public void runAway()
-	{
-		
-	}
-	
-    public void update()
-    {
-    	//player.animationUpdate();
-    	//player.update();
-    }
-    
-    public void oncePerSecondUpdate()
-    {
-    	//player.update();
-    }
-  
-    public void render(Graphics g)
-    {
-    	g.drawImage(backgroundImage, 0, 0, null);
-
-    	g.drawImage(player.getImage(), 3*GamePanel.WIDTH/4, GamePanel.HEIGHT/4, null);
-    	
-    	g.setColor(Color.lightGray);
-    	g.fillRect(GamePanel.WIDTH/2, 3*GamePanel.HEIGHT/4, GamePanel.WIDTH/2, GamePanel.HEIGHT/4);
-    	g.setColor(Color.orange);
-    	g.fillRect(0, 3*GamePanel.HEIGHT/4, GamePanel.WIDTH/2, GamePanel.HEIGHT/4);
-    	
-    	//Left Box
-    	g.setColor(Color.black);
-    	g.drawString("Name: " + player.getName(), 10, (3*GamePanel.HEIGHT/4)+10);
-    	g.drawString("HP: " + player.getHealth(), 10, (3*GamePanel.HEIGHT/4)+30);
-    	g.drawString("Mana: " + player.getMana(), 10, (3*GamePanel.HEIGHT/4)+50);
-    }
-    
-    public void drawOtherPlayers(Graphics g)
-    {
-    	
-    }
-    
-    public void drawLocations(Graphics g)
-    {
-    	
-    }
-  
-    public void onEnter()
-    {
-    	attackButton = new JButton("Attack");
+		//Sets up buttons for later use
+		attackButton = new JButton("Attack");
 		attackButton.setBounds(GamePanel.WIDTH/2 + 10, 2*GamePanel.HEIGHT/3 + 10, 300, 110);
 		attackButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -128,6 +68,82 @@ public class CombatState extends IState
             }
         });
 		
+	}
+	
+	public void attackMenu()
+	{
+		System.out.println("Attaaaaaaack");
+	}
+	
+	public void magicMenu()
+	{
+		System.out.println("PewPew");
+	}
+	
+	public void inventoryMenu()
+	{
+		System.out.println("Open Inventory");
+	}
+	
+	public void runAway()
+	{
+		System.out.println("Run away, little girl.");
+	}
+	
+    public void update()
+    {
+    	//player.animationUpdate();
+    	//player.update();
+    }
+    
+    public void oncePerSecondUpdate()
+    {
+    	//player.update();
+    }
+  
+    public void render(Graphics g)
+    {
+    	g.drawImage(backgroundImage, 0, 0, null);
+
+    	g.drawImage(player.getImage(), 3*GamePanel.WIDTH/4, GamePanel.HEIGHT/4, null);
+    	
+    	
+    	
+    	if(enemies.size() > 0)
+    	{
+    		g.setColor(Color.red);
+			for(int i = 0; i < enemies.size(); i++)
+			{
+				Rectangle enemyRect = enemies.get(i).getRect();
+				g.fillRect(enemyRect.x, enemyRect.y, enemyRect.width, enemyRect.height);
+			}
+    	}
+    	//Left Box Background
+    	g.setColor(Color.orange);
+    	g.fillRect(0, 2*GamePanel.HEIGHT/3, GamePanel.WIDTH/2, GamePanel.HEIGHT/3);
+    	//Right Box Background
+    	g.setColor(Color.lightGray);
+    	g.fillRect(GamePanel.WIDTH/2, 2*GamePanel.HEIGHT/3, GamePanel.WIDTH/2, GamePanel.HEIGHT/3);
+    	
+    	//Left Box Info
+    	g.setColor(Color.black);
+    	g.drawString("Name: " + player.getName(), 10, (3*GamePanel.HEIGHT/4)+10);
+    	g.drawString("HP: " + player.getHealth(), 10, (3*GamePanel.HEIGHT/4)+30);
+    	g.drawString("Mana: " + player.getMana(), 10, (3*GamePanel.HEIGHT/4)+50);
+    }
+    
+    public void drawOtherPlayers(Graphics g)
+    {
+    	
+    }
+    
+    public void drawLocations(Graphics g)
+    {
+    	
+    }
+  
+    public void onEnter()
+    {
 		sm.addComponent(attackButton);
 		sm.addComponent(magicButton);
 		sm.addComponent(inventoryButton);
@@ -146,7 +162,9 @@ public class CombatState extends IState
     	{
     		String[] thisEnemy = info[i].split(" ");
     		//Takes an enemies name, health, and damage (?)
-    		enemies.add(new Enemy(thisEnemy[0], Integer.parseInt(thisEnemy[1]), Integer.parseInt(thisEnemy[2])));
+    		Enemy newEnemy = new Enemy(thisEnemy[0], Integer.parseInt(thisEnemy[1]), Integer.parseInt(thisEnemy[2]));
+    		newEnemy.setRect(GamePanel.WIDTH/8, GamePanel.HEIGHT/8 + (i*100), Enemy.WIDTH, Enemy.HEIGHT);
+    		enemies.add(newEnemy);
     	}
     	onEnter();
     }
