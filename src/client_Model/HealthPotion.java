2f905bc98f16deee8ae00eb4c434e26230b41683
Joseph <joseph.a.gauthier@gmail.com>
2016-04-08 17:44:41 -0500

    Update HealthPotion.java

--- src/client_Model/HealthPotion.java
+++ src/client_Model/HealthPotion.java
@@ -4,18 +4,18 @@ public class HealthPotion extends Consumable{
 	
 	int level;
 
-	public HealthPotion(Player p, String n, int l) {
-		super(p, n);
-		level = l;
-		if(l == 1)
+	public HealthPotion(String name, Player player, int level) {
+		super(name, player);
+		this.level = level;
+		if(level == 1)
 		{
 			restoreAmount = 10;
 		}
-		else if(l == 2)
+		else if(level == 2)
 		{
 			restoreAmount = 50;
 		}
-		else if(l == 3)
+		else if(level == 3)
 		{
 			restoreAmount = 100;
 		}
