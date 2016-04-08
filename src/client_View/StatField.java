55f75ac612a43339e87620e61dfdaf9505b9b416
jhunt10 <jxh074@uark.edu>
2016-04-07 20:30:42 -0500

    Update StatField.java

--- src/client_View/StatField.java
+++ src/client_View/StatField.java
@@ -1,53 +1,72 @@
 package client_View;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class StatField {
 
 	JPanel panel;
 	JButton minus;
 	JButton plus;
-	JLabel points;
+	JTextField points;
 	JLabel label;
 	
 	public StatField(String l)
 	{
 		panel = new JPanel();
+		panel.setBackground(Launcher.TRANSPARENT);
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.NONE;
-        
-        label = new JLabel();
-		label.setText(l);
-		minus = new JButton();
-		plus = new JButton();
-		points = new JLabel();
-		points.setText("5");        		
+        c.fill = GridBagConstraints.NONE;    
+        c.anchor = GridBagConstraints.EAST;
 		
-		minus.setText("-");
-        minus.setPreferredSize(new Dimension(50, 50));
+		minus = new JButton();
+		minus.setIcon(new ImageIcon(Launcher.minusIcon));
+		minus.setBackground(Launcher.TRANSPARENT);
+		minus.setBorder(null);
+//		minus.setBounds(0,0,minus.getWidth(), minus.getHeight());
         minus.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 minusStat(evt);
             }
         });
         
-        plus.setText("+");
-        plus.setPreferredSize(new Dimension(50, 50));
+        points = new JTextField("5");
+        points.setEditable(false);
+        points.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        points.setBackground(Launcher.FIELDGRAY);
+		points.setMinimumSize(new Dimension(50,30));
+		points.setHorizontalAlignment(JTextField.CENTER);
+//		points.setBounds(minus.getWidth() + 10, 0, points.getWidth(), points.getHeight());
+        
+        plus = new JButton();
+        plus.setIcon(new ImageIcon(Launcher.plusIcon));
+        plus.setBackground(Launcher.TRANSPARENT);
+        plus.setBorder(null);
+//        plus.setBounds(minus.getWidth() + points.getWidth() + 20, 0, plus.getWidth(), plus.getHeight());
         plus.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 plusStat(evt);
             }
         });
         
+        label = new JLabel();
+		label.setText(l);  
+//		label.setPreferredSize(new Dimension(100, label.getHeight()));
+		
+		c.insets = new Insets(0,0,10,10);
+		
         c.gridx = 0;
         c.gridy = 0;
         panel.add(minus, c);
@@ -73,7 +92,7 @@ public class StatField {
     		curPoints--;
     		points.setText(String.valueOf(curPoints));
     		Launcher.pointsRemaining++;
-    		Launcher.pointsLeft.setText("Points remaining: " + Launcher.pointsRemaining);
+    		Launcher.pointsLeft.setText(String.valueOf(Launcher.pointsRemaining));
 		}
 	}
 	
@@ -85,7 +104,7 @@ public class StatField {
     		curPoints++;
     		points.setText(String.valueOf(curPoints));
     		Launcher.pointsRemaining--;
-    		Launcher.pointsLeft.setText("Points remaining: " + Launcher.pointsRemaining);
+    		Launcher.pointsLeft.setText(String.valueOf(Launcher.pointsRemaining));
 		}
 	}
 }
