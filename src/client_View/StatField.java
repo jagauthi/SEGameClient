0d5603595297ec6737dc2736c2f2ac381adb803c
jhunt10 <jxh074@uark.edu>
2016-04-10 12:57:25 -0500

    Update StatField.java

--- src/client_View/StatField.java
+++ src/client_View/StatField.java
@@ -16,25 +16,43 @@ import javax.swing.JTextField;
 
 public class StatField {
 
-	JPanel panel;
-	JButton minus;
-	JButton plus;
-	JTextField points;
-	JLabel label;
+	public static final int NEW = 0;
+	public static final int IN_GAME = 1;
+	public static final int LEVEL_UP = 2;
+	public JPanel panel;
 	
-	public StatField(String l)
+	private JButton minus;
+	private JButton plus;
+	private JTextField points, pointsToAdd, pointsLeft;
+	private JLabel label;
+	private ImageIcon plusIcon, minusIcon;
+	private StatField[] otherFields;
+	private int[] valueArray;
+	private int index, startValue, type;
+	
+	public StatField(String name, JTextField pointsLeftIn, int[] values, StatField[] othersIn, int indexin, int start, int typeIn)
 	{
+		valueArray = values;
+		index = indexin;
+		startValue = start;
+		pointsLeft = pointsLeftIn;
+		otherFields = othersIn;
+		type = typeIn;
+		
 		panel = new JPanel();
-		panel.setBackground(Launcher.TRANSPARENT);
+		panel.setBackground(Launcher.FAKETRANS);
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.NONE;    
         c.anchor = GridBagConstraints.EAST;
 		
+        minusIcon = new ImageIcon(Launcher.minusIcon);
+        plusIcon = new ImageIcon(Launcher.plusIcon);
+        
 		minus = new JButton();
-		minus.setIcon(new ImageIcon(Launcher.minusIcon));
-		minus.setBackground(Launcher.TRANSPARENT);
+		minus.setBackground(Launcher.FAKETRANS);
 		minus.setBorder(null);
+		minus.setMinimumSize(new Dimension(minusIcon.getIconWidth(), minusIcon.getIconHeight()));
 //		minus.setBounds(0,0,minus.getWidth(), minus.getHeight());
         minus.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -42,18 +60,27 @@ public class StatField {
             }
         });
         
-        points = new JTextField("5");
+        points = new JTextField(2);
         points.setEditable(false);
         points.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         points.setBackground(Launcher.FIELDGRAY);
 		points.setMinimumSize(new Dimension(50,30));
 		points.setHorizontalAlignment(JTextField.CENTER);
+		
+		pointsToAdd = new JTextField(3);
+		pointsToAdd.setText("+0");
+		pointsToAdd.setEditable(false);
+		pointsToAdd.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pointsToAdd.setBackground(Launcher.FIELDGRAY);
+		pointsToAdd.setMinimumSize(new Dimension(50,30));
+		pointsToAdd.setHorizontalAlignment(JTextField.CENTER);
 //		points.setBounds(minus.getWidth() + 10, 0, points.getWidth(), points.getHeight());
         
         plus = new JButton();
-        plus.setIcon(new ImageIcon(Launcher.plusIcon));
+        
         plus.setBackground(Launcher.TRANSPARENT);
         plus.setBorder(null);
+        plus.setMinimumSize(new Dimension(plusIcon.getIconWidth(), plusIcon.getIconHeight()));
 //        plus.setBounds(minus.getWidth() + points.getWidth() + 20, 0, plus.getWidth(), plus.getHeight());
         plus.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -62,49 +89,201 @@ public class StatField {
         });
         
         label = new JLabel();
-		label.setText(l);  
-//		label.setPreferredSize(new Dimension(100, label.getHeight()));
+		label.setText(name);  
+		//label.setMinimumSize(new Dimension(170, plusIcon.getIconHeight()));
 		
-		c.insets = new Insets(0,0,10,10);
-		
-        c.gridx = 0;
-        c.gridy = 0;
-        panel.add(minus, c);
-        
-        c.gridx = 1;
-        c.gridy = 0;
-        panel.add(points, c);
-        
-        c.gridx = 2;
-        c.gridy = 0;
-        panel.add(plus, c);
-        
-        c.gridx = 3;
-        c.gridy = 0;
-        panel.add(label, c);
+		if(type == NEW)
+		{
+			plus.setIcon(plusIcon);
+			minus.setIcon(minusIcon);
+			points.setText(String.valueOf(valueArray[index]));
+			
+			c.insets = new Insets(0,0,10,10);
+			c.anchor = c.EAST;
+			
+		    c.gridx = 0;
+		    c.gridy = 0;
+		    panel.add(minus, c);
+		    
+		    c.gridx = 1;
+		    c.gridy = 0;
+		    panel.add(points, c);
+		    
+		    c.gridx = 2;
+		    c.gridy = 0;
+		    panel.add(plus, c);
+		    
+		    c.gridx = 3;
+		    c.gridy = 0;
+		    panel.add(label, c);
+		}
+		else
+		{
+			plus.setIcon(plusIcon);
+			minus.setIcon(null);
+			points.setText(String.valueOf(start));
+			
+			c.insets = new Insets(0,0,10,10);
+			c.anchor = c.EAST;
+			
+		    c.gridx = 0;
+		    c.gridy = 0;
+		    panel.add(label, c);
+		    
+		    c.gridx = 1;
+		    c.gridy = 0;
+		    panel.add(points, c);
+		    
+		    c.gridx = 2;
+		    c.gridy = 0;
+		    panel.add(minus, c);
+		    
+		    c.gridx = 3;
+		    c.gridy = 0;
+		    panel.add(pointsToAdd, c);
+
+		    c.gridx = 4;
+		    c.gridy = 0;
+		    panel.add(plus, c);
+		}
+		if(type == IN_GAME)
+		{
+			plus.setIcon(null);
+			plus.setEnabled(false);
+			minus.setIcon(null);
+			minus.setEnabled(false);
+			pointsToAdd.setBackground(Launcher.TRANSPARENT);
+			pointsToAdd.setForeground(Launcher.TRANSPARENT);
+			pointsToAdd.setBorder(null);
+		}
+	}
+	
+	public void setPointValue(int n)
+	{
+		points.setText(String.valueOf(n));
 	}
 	
 	public void minusStat(ActionEvent evt)
 	{
-		int curPoints = Integer.parseInt(points.getText());
-		if(curPoints > 1)
+		if(valueArray[index] > 0)
 		{
-    		curPoints--;
-    		points.setText(String.valueOf(curPoints));
-    		Launcher.pointsRemaining++;
-    		Launcher.pointsLeft.setText(String.valueOf(Launcher.pointsRemaining));
+			if(type == NEW)
+			{
+				valueArray[index]--;
+	    		points.setText(String.valueOf(valueArray[index]));
+	    		valueArray[0]++;
+	    		pointsLeft.setText(String.valueOf(valueArray[0]));
+			}
+			else if(type == LEVEL_UP)
+			{
+				valueArray[index]--;
+	    		pointsToAdd.setText("+"+String.valueOf(valueArray[index]));
+	    		valueArray[0]++;
+	    		pointsLeft.setText(String.valueOf(valueArray[0]));
+			}
 		}
+		this.updateAll();
 	}
 	
 	public void plusStat(ActionEvent evt)
 	{
-		if(Launcher.pointsRemaining > 0)
+		if(valueArray[0] > 0)
 		{
-    		int curPoints = Integer.parseInt(points.getText());
-    		curPoints++;
-    		points.setText(String.valueOf(curPoints));
-    		Launcher.pointsRemaining--;
-    		Launcher.pointsLeft.setText(String.valueOf(Launcher.pointsRemaining));
+			if(type == NEW)
+			{
+				valueArray[index]++;
+	    		points.setText(String.valueOf(valueArray[index]));
+	    		valueArray[0]--;
+	    		pointsLeft.setText(String.valueOf(valueArray[0]));
+			}
+			else if(type == LEVEL_UP)
+			{
+				valueArray[index]++;
+	    		pointsToAdd.setText("+"+String.valueOf(valueArray[index]));
+	    		valueArray[0]--;
+	    		pointsLeft.setText(String.valueOf(valueArray[0]));
+			}
+		}
+		this.updateAll();
+	}
+	public void updateAll()
+	{
+		for(int n=0; n < otherFields.length; n++)
+		{
+			otherFields[n].update();
+		}
+	}
+	
+	public void update()
+	{
+		if(valueArray[0] <= 0 | type == IN_GAME)
+		{
+			plus.setIcon(null);
+			plus.setEnabled(false);
+		}
+		else if(plus.getIcon() == null)
+		{
+			plus.setIcon(plusIcon);
+			plus.setEnabled(true);
+		}
+		
+		if(valueArray[index] <= 0 | type == IN_GAME)
+		{
+			minus.setIcon(null);
+			minus.setEnabled(false);
+		}
+		else if(minus.getIcon() == null)
+		{
+			minus.setIcon(minusIcon);
+			minus.setEnabled(true);
+		}
+		panel.repaint();
+	}
+	
+	public String getStringValue()
+	{
+		if(type == NEW)
+		{
+			return points.getText();
+		} 
+		else 
+		{
+			return pointsToAdd.getText();
+		}
+	}
+	
+	public void reset(){
+		pointsToAdd.setText("+"+String.valueOf(valueArray[index]));
+	}
+	
+	public void showPlus(boolean b)
+	{
+		if(b)
+		{
+			plus.setIcon(plusIcon);
+			plus.setEnabled(true);
+			if(valueArray[index] == 0)
+			{
+				minus.setIcon(null);
+			}
+			else
+			{
+				minus.setIcon(minusIcon);
+			}
+			minus.setEnabled(true);
+			pointsToAdd.setBackground(Launcher.FIELDGRAY);
+			pointsToAdd.setForeground(label.getForeground());
+			pointsToAdd.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		}
+		else
+		{
+			plus.setIcon(null);
+			plus.setEnabled(false);
+			minus.setIcon(null);
+			minus.setEnabled(false);
+			pointsToAdd.setBackground(Launcher.TRANSPARENT);
+			pointsToAdd.setForeground(Launcher.TRANSPARENT);
+			pointsToAdd.setBorder(null);
 		}
 	}
 }
